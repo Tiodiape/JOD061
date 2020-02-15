6c21a3757ad5ab1c641d8daa31052a5bff9ca9c1
unknown <1814070008@academico.iesb.br>
2020-02-15 10:40:29 -0300

    botei parentese

--- Aulas/Aula01/OlaMundo.cs
+++ Aulas/Aula01/OlaMundo.cs
@@ -1,14 +1,10 @@
 using System;
 
 Class programa {
-<<<<<<< HEAD
-    //alooooooo
 
+    //alooooooo
     
-}
-=======
-     
-    int main{
+    int main(){
     }
 }
->>>>>>> b4dd49275814ab00d48121c13891d0027f0238ba
+
